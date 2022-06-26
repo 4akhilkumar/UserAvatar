3540311a87f8e8b145a943691cca797c8defb8fd
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2022-06-26 19:29:29 +0530

    User Avatar using JavaScript and CSS

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # UserAvatar
 User Avatar using JavaScript and CSS
+
+## Integrate UserAvatar
+### Import *UserAvatar Javascript* and place it in body of your HTML file
+```
+<script src="https://cdn.rawgit.com/4akhilkumar/UserAvatar/master/index.js"></script>
+```
+### To display the UserAvatar write the below code
+```
+<div class="avatar" data-label="<Username>"></div>
+``` 
+
+### Output
+![MarineGEO circle logo](https://raw.githubusercontent.com/4akhilkumar/ToastMessageJS/master/Screenshot%202022-06-26%20192710.jpg "ToastMessageJS")
+
+### Reference
+[Easily Generate User Avatars for Your Website with CSS](https://www.youtube.com/watch?v=o8d-bjPzI-g)
---
+++ index.js
@@ -0,0 +1,39 @@
+let avatarContainer;
+
+(function initAvatar() {
+  document.body.insertAdjacentHTML('afterbegin', 
+  `<style>
+
+    .avatar {
+        font-size: 16px;
+        width: 3em;
+        height: 3em;
+        border-radius: 50%;
+        color: #FFF;
+        background-color: #E93B81;
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .avatar::after {
+        content: attr(data-label);
+        font-family: sans-serif;
+    }
+
+  </style>`);
+  avatarContainer = document.querySelector('.avatar-container');
+})()
+
+const avatars = document.querySelectorAll('.avatar');
+
+avatars.forEach(avatar => {
+    const charCodeRed = avatar.dataset.label.charCodeAt(0);
+    const charCodeGreen = avatar.dataset.label.charCodeAt(1) || charCodeRed;
+    
+    const red = Math.pow(charCodeRed, 7) % 200; // To avoid colliding with font-color
+    const green = Math.pow(charCodeGreen, 7) % 200; // To avoid colliding with font-color
+    const blue = (red + green) % 200; // To avoid colliding with font-color
+
+    avatar.style.background = `rgb(${red}, ${blue}, ${green})`;
+})
